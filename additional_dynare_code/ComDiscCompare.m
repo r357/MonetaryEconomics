594f248aba940960778205c83e1745418f84b685
Alen Rožac <32488308+r357@users.noreply.github.com>
2018-11-26 15:43:28 +0100

    Add files via upload

--- additional_dynare_code/ComDiscCompare.m
+++ additional_dynare_code/ComDiscCompare.m
@@ -15,9 +15,9 @@ IRcommit_y_ea=oo_.irfs.y_ea;
 IRcommit_pi_ea=oo_.irfs.pi_ea;
 IRcommit_y_pi=oo_.irfs.y_epi;
 IRcommit_pi_pi=oo_.irfs.pi_epi;
-VarYcommit=oo_.var(1,1);% Variance of output
+VarYcommit=oo_.var(5,5);% Variance of output
 VarPicommit=oo_.var(3,3);% Variance of inflation
-LossCommit = VarYcommit+lambda*VarPicommit;
+LossCommit = VarPicommit+lambda*VarYcommit;
 
 % Solve the model under discretion and collect results
 dynare NKmod_disc noclearall
@@ -26,9 +26,9 @@ IRdisc_y_ea=oo_.irfs.y_ea;
 IRdisc_pi_ea=oo_.irfs.pi_ea;
 IRdisc_y_pi=oo_.irfs.y_epi;
 IRdisc_pi_pi=oo_.irfs.pi_epi;
-VarYdisc=oo_.var(1,1);% Variance of output
+VarYdisc=oo_.var(5,5);% Variance of output
 VarPidisc=oo_.var(3,3);% Variance of inflation
-LossDisc = VarYdisc+lambda*VarPidisc;
+LossDisc = VarPidisc+lambda*VarYdisc;
 
 display('Loss under commitment:');; LossCommit
 display('Loss under discretion:');; LossDisc
